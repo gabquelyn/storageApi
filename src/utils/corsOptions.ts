8e1a4f197cd3e0fbc9cfc1374b57822e59741db2
LoneDev <gabquelyn@gmail.com>
2024-03-23 13:23:54 +0000

    added development origin

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -2,14 +2,19 @@ import { CorsOptions } from "cors";
 
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    if (["http://localhost:3000"].includes(origin as string) || !origin) {
+    if (
+      ["http://localhost:3000", "https://storageapp-two.vercel.app"].includes(
+        origin as string
+      ) ||
+      !origin
+    ) {
       callback(null, true); //first is error which is null and second allow wich is true
     } else {
       callback(new Error("Not allowed by cors"));
     }
   },
   credentials: true,
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
 };
 
 export default corsOptions;
